a37a5d8e9dbcd722a52063b376a57c635319d123
Novailoveyou <novailoveyoux@gmail.com>
2021-12-03 14:15:09 +0300

    breakpoints can now be changed in one place in sass and be imported in ts

--- config/breakpoints.ts
+++ config/breakpoints.ts
@@ -1,7 +1,16 @@
-const breakpoints = {
-  phone: 480,
-  tablet: 768,
-  laptop: 1200
+// @ts-ignore
+import { phone, tablet, laptop } from '@/styles/base/Breakpoints.module.sass'
+
+type TypeBreakpoints = {
+  phone: number
+  tablet: number
+  laptop: number
+}
+
+const breakpoints: TypeBreakpoints = {
+  phone: +phone.slice(0, -2),
+  tablet: +tablet.slice(0, -2),
+  laptop: +laptop.slice(0, -2)
 }
 
 export default breakpoints
