6fc420d6462a9c03b1d8f77c3587d72c1b10c8b9
Novailoveyou <novailoveyoux@gmail.com>
2022-02-23 13:47:49 +0300

    changed HeroPicutre styles to make new hero image look good

--- components/imgs/general/ImgHero.tsx
+++ components/imgs/general/ImgHero.tsx
@@ -1,13 +1,13 @@
 import stls from '@/styles/components/imgs/general/ImgHero.module.sass'
 import Image from 'next/image'
-import pic from '@/public/assets/imgs/general/lady-studying.jpg'
+import pic from '@/public/assets/imgs/general/hero.jpg'
 
 const ImgHero = ({ width = 0, height = 0 }) => {
   return (
     <div className={stls.container}>
       <Image
         src={pic}
-        alt='Девушка за ноутбуком обучается'
+        alt='Сферы медицины'
         className={stls.img}
         width={width !== 0 && width}
         height={height !== 0 && height}
